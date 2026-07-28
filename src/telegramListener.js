e8bff2f25424e027cf8523a6d6dad121e2221713
Kartik Singh <kartiksinghjaipur@gmail.com>
2026-07-28 19:31:24 +0530

    telegram error polling

--- src/telegramListener.js
+++ src/telegramListener.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
 const axios = require("axios");
 
 const TelegramBot = require("node-telegram-bot-api");
